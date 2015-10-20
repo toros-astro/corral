56370c11ef21c046b3f75732016015171e8ba61d
Juan B Cabral <jbc.develop@gmail.com>
2015-10-20 00:36:11 -0300

    remove patch

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ.setdefault("CORRAL_SETTINGS_MODULE", "toritos.settings")
-from corral.core import setup_environment
-setup_environment()
-
--- run.py
+++ run.py
@@ -4,7 +4,8 @@
 import os
 import sys
 
-os.environ.setdefault("CORRAL_SETTINGS_MODULE", "toritos.settings")
+if __name__ == "__main__":
+    os.environ.setdefault("CORRAL_SETTINGS_MODULE", "toritos.settings")
 
-from corral import cli
-cli.run_from_command_line(sys.argv[1:])
+    from corral import cli
+    cli.run_from_command_line(sys.argv[1:])
