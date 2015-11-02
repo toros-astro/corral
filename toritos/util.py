b9363970ad18204ec288b06f7a16268326d1d48a
Bruno Sanchez <bruno@oac.unc.edu.ar>
2015-11-02 10:46:50 -0300

    Modified toritos util with simple docstrings

---
+++ toritos/util.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module with simple tools for specific
+toritos data handling routines.
+"""
+
+
+def scandir(path):
+    """Tool for scanning dirs and returning
+    all the fits files located on them.
+    """
+    return None
