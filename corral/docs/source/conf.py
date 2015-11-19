adffc15d558054ad39d98f947cf41881300425c5
brunosanchez <bruno@oac.unc.edu.ar>
2015-11-19 12:42:58 -0300

    Trying to use Readthedocs sphinx

--- corral/docs/source/conf.py
+++ corral/docs/source/conf.py
@@ -21,10 +21,16 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+from recommonmark.parser import CommonMarkParser
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -40,8 +46,8 @@ templates_path = ['.templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
