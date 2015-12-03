1656df415a830e5c6c2bcaf1542f0bc0ff0010b4
brunosanchez <bruno@oac.unc.edu.ar>
2015-12-03 13:28:51 -0300

    Path implementation

--- toritos/load.py
+++ toritos/load.py
@@ -8,14 +8,23 @@ from toritos import models, util
 
 class Load(run.Loader):
     def setup(self):
-        self.rawstate = self.session.query(models.State).filter(models.State.name == "raw_data").first()
-        self.session.autocommit = True
+        self.rawstate = self.session.query(
+            models.State).filter(models.State.name == "raw").first()
+        self.session.autocommit = False
+        self.buff = []
 
     def generate(self):
-        pawprints = util.scandir(settings.PAWPRINTPATH)
+        pawprints = util.scandir(settings.PAWPRINT_PATH)
         for afile in pawprints:
             data = util.fitsparser(afile)
             print data
             paw = models.Pawprint(**data)
             paw.state_id = self.rawstate.id
+            self.buff.append((afile, paw))
             yield paw
+
+    def teardown(self, type, value, traceback):
+        if not type:
+            self.session.commit()
+            for afile, paw in self.buff:
+                paw.writefile(afile, self.rawstate)
