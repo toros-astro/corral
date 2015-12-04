586f756e5308021c4044e50b9e50cc4c5b709a14
Juan B Cabral <jbc.develop@gmail.com>
2015-12-04 00:27:35 -0300

    end alerts

--- corral/run/alert.py
+++ corral/run/alert.py
@@ -4,8 +4,12 @@
 import abc
 import inspect
 import multiprocessing
+import datetime
+import collections
+import copy
 
 from .. import conf, db, util, exceptions
+from ..db.default_models import Alerted
 from ..core import logger
 
 from .base import Processor, Runner
@@ -28,7 +32,13 @@ class AlertRunner(Runner):
         with db.session_scope() as session, alert_cls(session) as alert:
             for obj in alert.generate():
                 alert.validate(obj)
-                alert.process(obj)
+
+                generator = alert.process(obj) or []
+                if not hasattr(generator, "__iter__"):
+                    generator = (generator,)
+                for proc_obj in generator:
+                    alert.validate(proc_obj)
+                    alert.save(proc_obj)
         logger.info("Done!")
 
 
@@ -42,6 +52,8 @@ class Alert(Processor):
     ordering = None
     offset, limit = None, None
 
+    auto_register = True
+
     def setup(self):
         map(lambda ep: ep.setup(self), self.alert_to)
 
@@ -54,7 +66,24 @@ class Alert(Processor):
             raise NotImplementedError(
                 "'{}' subclass with a default generate must redefine "
                 "'model' and 'conditions' class-attributes".format(clsname))
+
         query = self.session.query(self.model).filter(*self.conditions)
+
+        if self.auto_register:
+            filters = Alerted.alert_to_columns(type(self))
+            filters.update(Alerted.model_class_to_column(self.model))
+            alerteds = self.session.query(
+                Alerted.model_ids).filter_by(**filters)
+            if alerteds.count():
+                grouped_id = collections.defaultdict(set)
+                for d in map(lambda r: Alerted.loads(r[0]), alerteds.all()):
+                    for k, v in d.iteritems():
+                        grouped_id[k].add(v)
+                exclude = []
+                for k, v in grouped_id.items():
+                    exclude.append(getattr(self.model,k).in_(v))
+                query = query.filter(~db.and_(*exclude))
+
         if self.ordering is not None:
             query = query.order_by(*self.ordering)
         if self.offset is not None:
@@ -65,6 +94,12 @@ class Alert(Processor):
 
     def process(self, obj):
         map(lambda ep: ep.process(obj), self.alert_to)
+        if self.auto_register:
+            register = Alerted()
+            register.alert = type(self)
+            register.model = obj
+            register.created_at = datetime.datetime.utcnow()
+            yield register
 
 
 # =============================================================================
